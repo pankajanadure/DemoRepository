dbbdc0dd840a23e211c7d14a6c121ec171f606a3
pankajanadure <pankajanadure@gmail.com>
2021-03-24 17:12:31 +0530

    Altered Abc.java and Constructed Xyz.java

--- Abc.java
+++ Abc.java
@@ -3,5 +3,9 @@ public class Abc
 	public static void main(String args[])
 	{
 		System.out.println("HELLO");
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("HI");
+		}
 	}
 }
---
+++ Xyz.java
@@ -0,0 +1,15 @@
+public class Xyz
+{
+	private int x;
+	private String str;
+	public void setStr()
+	{
+	}
+	public void setX()
+	{
+	}
+	public int getX()
+	{
+		return x;
+	}
+}
